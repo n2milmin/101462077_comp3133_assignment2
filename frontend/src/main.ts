06db6ddf3305afc203f6e250ee54c18a83aead67
n2milmin <nicoleamilmine@gmail.com>
2025-04-06 11:04:47 -0400

    imported angular/material

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideApollo } from 'apollo-angular';
+import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const apolloOptions: ApolloClientOptions<any> = {
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+};
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideApollo(() => apolloOptions), 
+    provideHttpClient(),
+    provideRouter(routes),
+  ],
+}).catch((err) => console.error(err));
